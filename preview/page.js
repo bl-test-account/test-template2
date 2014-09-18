1950e6e17a987f4330b5961f638c1745d9eb2aac
bl-test-account <plugin.developer@bytelight.com>
2014-09-18 16:28:01 -0400

    Released on: 2014-09-18 16:28:00 -0400

---
+++ preview/page.js
@@ -0,0 +1,16 @@
+/*
+Get the current location and user information by calling
+BL.getContext.The returned object contains the lightGroupId,
+lightGroupName, and userId properties.
+*/
+context = BL.getContext();
+$('#description').text('Hello World: This is the preview page that gets used to preview your app. Click to view the display page of your app');
+$('#lightGroupId').text(context.lightGroupId);
+$('#lightGroupName').text(context.lightGroupName);
+$('#userId').text(context.userId);
+
+// Register a click handler on the entire preview
+$('#container').on('click', function() {
+  // Open the show page by calling BL.displayApp(APP.appId).
+  BL.displayApp(APP.appId);
+});
