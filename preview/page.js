93292fe5923069d9a470908f1a6b6e32728010a0
bl-test-account <plugin.developer@bytelight.com>
2014-09-25 18:50:37 -0400

    Released on: 2014-09-25 18:50:36 -0400

--- preview/page.js
+++ preview/page.js
@@ -4,17 +4,3 @@ BL.getContext.The returned object contains the lightGroupId,
 lightGroupName, and userId properties.
 */
 context = BL.getContext();
-$('#description').text('Hello World: This is the preview page that gets used to preview your app. Click to view the display page of your app');
-$('#lightGroupId').text(context.lightGroupId);
-$('#lightGroupName').text(context.lightGroupName);
-$('#userId').text(context.userId);
-
-BL.addTemplateResetFunction(function() {
-  $('p').css('color', 'blue')
-})
-
-// Register a click handler on the entire preview
-$('#container').on('click', function() {
-  // Open the show page by calling BL.displayApp(APP.appId).
-  //BL.displayApp(APP.appId);
-});
